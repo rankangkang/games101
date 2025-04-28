43110285d018105306ecc22f2bc87781dbf31cd7
rankangkang <rankangkang@foxmail.com>
2025-04-28 14:09:15 +0800

    feat: optimize

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import type { IDBOptions } from './db/idb'
 export const ROUTE_BASE_NAME = '/games101'
 
 export const SERVICE_WORKER_PATH = `${ROUTE_BASE_NAME}/sw.js`
+export const SERVICE_WORKER_SCOPE = `${ROUTE_BASE_NAME}/`
 export const ASSIGNMENTS_BASE_PREFIX = `${ROUTE_BASE_NAME}/_assignments`
 
 export const IDB_OPTIONS: IDBOptions = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ROUTE_BASE_NAME, SERVICE_WORKER_PATH } from './config'
+import { SERVICE_WORKER_PATH, SERVICE_WORKER_SCOPE } from './config'
 import { idb } from './db'
 import { Router } from './router'
 import './styles/style.css'
 import { registryServiceWorker } from './utils/registryServiceWorker'
 
-registryServiceWorker(SERVICE_WORKER_PATH, ROUTE_BASE_NAME).then(async () => {
+registryServiceWorker(SERVICE_WORKER_PATH, SERVICE_WORKER_SCOPE).then(async () => {
   await idb.getDB()
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
